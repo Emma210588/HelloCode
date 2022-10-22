dfc56adad8400759cf3ace8d3d1dd65df7332853
Dulebova Emma <emma2105@yandex.ru>
2022-10-22 12:46:03 +0300

    add Example 011 ArrayLibrary

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,27 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+
+void PrintArray(int[] col) // невозвращаемый метод
+{
+    int count = col.Length; // количество элементов
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int[] array = new int[10]; // создай новый массив, в котором 10 элементов
+
+FillArray(array);
+PrintArray(array);
