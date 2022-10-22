b3fbd01dc55a7c6f30d7f41135dcc8b8ecc32b56
Dulebova Emma <emma2105@yandex.ru>
2022-10-22 13:07:16 +0300

    add Example 011 Array Library position

--- Example011_ArrayLibrary/Program.cs
+++ Example011_ArrayLibrary/Program.cs
@@ -21,7 +21,31 @@ void PrintArray(int[] col) // невозвращаемый метод
     }
 }
 
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
 int[] array = new int[10]; // создай новый массив, в котором 10 элементов
 
 FillArray(array);
+array[4] = 4;
+array[6] = 4;
+
 PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
